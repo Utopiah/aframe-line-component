f0b385b01283d43ce5d4d443b23e99b58bb38bb1
Fernando Serrano <fernandojsg@gmail.com>
2017-03-27 18:30:33 +0200

    First commit

---
+++ dist/aframe-line-component.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	/* global AFRAME */
+
+	if (typeof AFRAME === 'undefined') {
+	  throw new Error('Component attempted to register before AFRAME was available.');
+	}
+
+	/**
+	 * Line component for A-Frame.
+	 */
+	AFRAME.registerComponent('line', {
+	  schema: {
+	    start: {type: 'vec3', default: '0 0 0'},
+	    end: {type: 'vec3', default: '0 0 0'},
+	    color: {type: 'color', default: '#fff'}
+	  },
+
+	  /**
+	   * Called once when component is attached. Generally for initial setup.
+	   */
+	  init: function () {
+	    var material = this.material = new THREE.LineBasicMaterial({color: this.data.color});
+	    var geometry = this.geometry = new THREE.BufferGeometry();
+	    geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(2 * 3), 3));
+
+	    this.line = new THREE.Line(geometry, material);
+	    this.el.setObject3D('line', this.line);
+	  },
+
+	  /**
+	   * Called when component is attached and when component data changes.
+	   * Generally modifies the entity based on the data.
+	   */
+	  update: function (oldData) {
+	    var data = this.data;
+	    var diff = AFRAME.utils.diff(data, oldData);
+
+	    // Update geometry
+	    if ('start' in diff || 'end' in diff) {
+	      var pos = this.geometry.attributes.position.array;
+	      pos[0] = data.start.x;
+	      pos[1] = data.start.y;
+	      pos[2] = data.start.z;
+	      pos[3] = data.end.x;
+	      pos[4] = data.end.y;
+	      pos[5] = data.end.z;
+	      this.geometry.attributes.position.needsUpdate = true;
+	    }
+
+	    this.material.color.setStyle(data.color);
+	  }
+	});
+
+
+/***/ }
+/******/ ]);
